b94951c984b58ddd74bc8e69ee6c4d960e501500
Devesh <deveshcc@gmail.com>
2022-11-17 10:55:15 +0530

    Update assets.js

--- app/lib/assets.js
+++ app/lib/assets.js
@@ -1,74 +1,74 @@
-export const WorkIcon = require('./../assets/img/workplace.png');
+export const WorkIcon = require('../assets/img/workplace.png');
 
-export const SkillsIcon = require('./../assets/img/pencil.png');
+export const SkillsIcon = require('../assets/img/pencil.png');
 
-export const EducationIcon = require('./../assets/img/education.png');
+export const EducationIcon = require('../assets/img/education.png');
 
-export const BooksIcon = require('./../assets/img/homework.png');
+export const BooksIcon = require('../assets/img/homework.png');
 
-export const BitVedasLogo = require('./../assets/img/BitVedas_logo.png');
-export const EphrineAppsLogo = require('./../assets/img/Ephrine-Apps_logo.png');
-export const EternalBlizzLogo = require('./../assets/img/Eternal-Blizz_logo.png');
+export const BitVedasLogo = require('../assets/img/BitVedas_logo.png');
+export const EphrineAppsLogo = require('../assets/img/Ephrine-Apps_logo.png');
+export const EternalBlizzLogo = require('../assets/img/Eternal-Blizz_logo.png');
 
-export const HeaderImgLogo = require('./../assets/img/header.jpg');
+export const HeaderImgLogo = require('../assets/img/header.jpg');
 
-export const DevSuper = require('./../assets/img/dev_super.png');
+export const DevSuper = require('../assets/img/dev_super.png');
 
-export const BlankImg = require('./../assets/img/blank.jpg');
-export const BlankImg1 = require('./../assets/img/blank1.jpg');
-
-
-export const Adobe_Ae_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Adobe_Ae.png');
-export const Adobe_Ai_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Adobe_Ai.png');
-export const Adobe_Au_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Adobe_Au.png');
-export const Adobe_Pr_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Adobe_Pr.png');
-
-export const Adobe_Ps_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Adobe_Ps.png');
-export const Android_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Android.png');
-export const Blender_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Blender.png');
-export const C_Plus_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_C_Plus.png');
-export const C_Sharp_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_C_Sharp.png');
-export const HTML5_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_HTML5.png');
-export const Java_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Java.png');
-export const NextJS_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_NextJs.png');
-export const NodeJS_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_NodeJs.png');
-export const ReactJs_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_ReactJs.png');
-export const Rx_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Rx.png');
-export const Sass_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Sass.png');
-export const Unity3d_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Unity-3D.png');
-export const WebDev_Skill_Badge = require('./../assets/img/skill_badges/skills_badge_Web-Dev.png');
-
-export const Work_Dev = require('./../assets/img/work_dev.png');
+export const BlankImg = require('../assets/img/blank.jpg');
+export const BlankImg1 = require('../assets/img/blank1.jpg');
 
 
-export const Indra_App_Icon = require('./../assets/img/product_icons/IndraIcon500px.png');
-export const PharmaHub_App_Icon = require('./../assets/img/product_icons/pharmahub.png');
-export const QRLite_App_Icon = require('./../assets/img/product_icons/qrlite-app-Icon512px.png');
-export const SMSDrive_App_Icon = require('./../assets/img/product_icons/sms-drive-icon.png');
-export const TextMaster_App_Icon = require('./../assets/img/product_icons/textmaster_icon512pc.png');
+export const Adobe_Ae_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Adobe_Ae.png');
+export const Adobe_Ai_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Adobe_Ai.png');
+export const Adobe_Au_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Adobe_Au.png');
+export const Adobe_Pr_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Adobe_Pr.png');
 
-export const Dev_Skills = require('./../assets/img/dev_skills.png');
+export const Adobe_Ps_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Adobe_Ps.png');
+export const Android_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Android.png');
+export const Blender_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Blender.png');
+export const C_Plus_Skill_Badge = require('../assets/img/skill_badges/skills_badge_C_Plus.png');
+export const C_Sharp_Skill_Badge = require('../assets/img/skill_badges/skills_badge_C_Sharp.png');
+export const HTML5_Skill_Badge = require('../assets/img/skill_badges/skills_badge_HTML5.png');
+export const Java_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Java.png');
+export const NextJS_Skill_Badge = require('../assets/img/skill_badges/skills_badge_NextJs.png');
+export const NodeJS_Skill_Badge = require('../assets/img/skill_badges/skills_badge_NodeJs.png');
+export const ReactJs_Skill_Badge = require('../assets/img/skill_badges/skills_badge_ReactJs.png');
+export const Rx_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Rx.png');
+export const Sass_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Sass.png');
+export const Unity3d_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Unity-3D.png');
+export const WebDev_Skill_Badge = require('../assets/img/skill_badges/skills_badge_Web-Dev.png');
 
-export const Dev_Cowboy_Hat=require('./../assets/img/devesh-cowboy-hat_Circle-Pic.png');
-
-export const GitHub_Octacat=require('./../assets/img/Octocat.jpg');
-
-export const AndroidDev_Banner=require('./../assets/img/RAW/pexels-lisa-fotios-1092644.jpg');
-export const WebDev_Banner=require('./../assets/img/RAW/pexels-pixabay-270360.jpg');
-export const BusinessDev_Banner=require('./../assets/img/RAW/pexels-fauxels-3184418.jpg');
-export const GameDev_Banner=require('./../assets/img/RAW/pexels-pixabay-459762.jpg');
-export const VideoEditing_Banner=require('./../assets/img/RAW/pexels-donald-tong-66134.jpg');
-
-export const ContentCreation_Banner=require('./../assets/img/RAW/pexels-picjumbocom-196644.jpg');
+export const Work_Dev = require('../assets/img/work_dev.png');
 
 
-export const PharmaDrug_Banner=require('./../assets/img/RAW/lemon-gae1119a8f_1280.jpg');
+export const Indra_App_Icon = require('../assets/img/product_icons/IndraIcon500px.png');
+export const PharmaHub_App_Icon = require('../assets/img/product_icons/pharmahub.png');
+export const QRLite_App_Icon = require('../assets/img/product_icons/qrlite-app-Icon512px.png');
+export const SMSDrive_App_Icon = require('../assets/img/product_icons/sms-drive-icon.png');
+export const TextMaster_App_Icon = require('../assets/img/product_icons/textmaster_icon512pc.png');
 
-export const Learning_Banner=require('./../assets/img/RAW/learning-gd2477fd86_1280.jpg');
+export const Dev_Skills = require('../assets/img/dev_skills.png');
 
-export const AcademicsImg=require('./../assets/img/RAW/books-g67c2b3a3d_1280.jpg');
+export const Dev_Cowboy_Hat=require('../assets/img/devesh-cowboy-hat_Circle-Pic.png');
 
-export const TheEnchantressBookImg=require('./../assets/img/the_enchantress_devesh.jpg');
+export const GitHub_Octacat=require('../assets/img/Octocat.jpg');
+
+export const AndroidDev_Banner=require('../assets/img/RAW/pexels-lisa-fotios-1092644.jpg');
+export const WebDev_Banner=require('../assets/img/RAW/pexels-pixabay-270360.jpg');
+export const BusinessDev_Banner=require('../assets/img/RAW/pexels-fauxels-3184418.jpg');
+export const GameDev_Banner=require('../assets/img/RAW/pexels-pixabay-459762.jpg');
+export const VideoEditing_Banner=require('../assets/img/RAW/pexels-donald-tong-66134.jpg');
+
+export const ContentCreation_Banner=require('../assets/img/RAW/pexels-picjumbocom-196644.jpg');
+
+
+export const PharmaDrug_Banner=require('../assets/img/RAW/lemon-gae1119a8f_1280.jpg');
+
+export const Learning_Banner=require('../assets/img/RAW/learning-gd2477fd86_1280.jpg');
+
+export const AcademicsImg=require('../assets/img/RAW/books-g67c2b3a3d_1280.jpg');
+
+export const TheEnchantressBookImg=require('../assets/img/the_enchantress_devesh.jpg');
 
 
 
